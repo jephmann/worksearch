1dc5fc157260db3eb24e83f4493abad1421eb320
jephmann <jephmann@gmail.com>
2013-09-11 21:24:23 -0500

    new functions

--- _functions/all.php
+++ _functions/all.php
@@ -4,16 +4,19 @@
     require_once ($page['path'].'_functions/read.php');
     require_once ($page['path'].'_functions/update.php');
     require_once ($page['path'].'_functions/delete.php');
-    // OTHER
-    require_once ($page['path'].'_functions/ddlDates.php');
-    require_once ($page['path'].'_functions/ddlOptions.php');
+    // STRINGS
     require_once ($page['path'].'_functions/formatEmailLink.php');
     require_once ($page['path'].'_functions/formatOutsideLink.php');
     require_once ($page['path'].'_functions/formatPhone.php');
     require_once ($page['path'].'_functions/formatPostUS.php');
+    // INPUTS
+    require_once ($page['path'].'_functions/ddlDates.php');
+    require_once ($page['path'].'_functions/ddlOptions.php');
     require_once ($page['path'].'_functions/rblGender.php');
     require_once ($page['path'].'_functions/rblOptions.php');
     require_once ($page['path'].'_functions/rblTrueFalse.php');
+    // OTHER
     require_once ($page['path'].'_functions/redirect.php');
     require_once ($page['path'].'_functions/returnAlreadyCheck.php');
     require_once ($page['path'].'_functions/returnSort.php');
+    require_once ($page['path'].'_functions/returnTHEAD.php');
---
+++ _functions/returnTHEAD.php
@@ -0,0 +1,28 @@
+<?php
+    function return_THEAD($array_columns)
+    {
+        $thead  = "<thead>";
+        $asc    = NULL;
+        $desc   = NULL;
+        $field  = NULL;
+        $title  = NULL;
+        for($i=0; $i<count($array_columns); $i++)
+        {
+            $title  = $array_columns[$i]['title'];
+            $thead  .= "<th>{$title}";
+            if($title=="OPTIONS")
+            {
+                $thead  .= "<br /><a title=\"Reset Table\" href=\"\">- Reset -</a>";
+            }
+            if(!empty($array_columns[$i]['field']))
+            {
+                $field  = $array_columns[$i]['field'];
+                $desc   = "<a title=\"{$title} Descending\" href=\"?orderby={$field}&dir=DESC\">&darr;</a>";
+                $asc    = "<a title=\"{$title} Ascending\" href=\"?orderby={$field}&dir=ASC\">&uarr;</a>";
+                $thead  .= "<br />{$desc}&nbsp;{$asc}";
+            }
+            $thead  .= "</th>";
+        }
+        $thead  .= "</thead>";
+        return $thead;
+    }
