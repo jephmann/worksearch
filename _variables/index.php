11ac0381364e9b555f50e67eb6c7555f0c34a3ae
jephmann <jephmann@gmail.com>
2013-09-23 00:11:44 -0500

    more included required files

---
+++ _variables/index.php
@@ -0,0 +1,10 @@
+<?php
+    $columns    = NULL;
+    $thead      = NULL;
+    $tbody      = NULL; 
+    $get        = FALSE;
+    $orderby    = NULL;
+    $dir        = NULL;
+    $and        = NULL;
+    $where      = NULL;
+    $like       = NULL;
