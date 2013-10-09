e332869a851e1f3a9e6dd0c81657d5c26ee4b058
jephmann <jephmann@gmail.com>
2013-10-09 12:12:13 -0500

    navigation tweaks

--- _views/header.php
+++ _views/header.php
@@ -11,30 +11,37 @@
             <h2><?php echo ($page['title'].$page['subtitle']); ?></h2>
         </header>
         <div class="clear" />
-        <nav>
+        <nav>            
+            <?php
+                $nav_li = NULL;
+                if (!empty($_SESSION['user']))
+                {
+                    $nav_li .= "<li><a href=\"{$page['path']}welcome.php\">HOME</a></li>
+                        <li><a href=\"{$page['path']}profile/\">Profile</a></li>";
+                }
+                if (!empty($_SESSION['profile']))
+                {
+                    $nav_li .= "<li><a href=\"{$page['path']}companies/\">Companies</a></li>
+                        <li><a href=\"{$page['path']}contacts/\">Contacts</a></li>
+                        <li><a href=\"{$page['path']}logs/\">Logs</a></li>
+                        <li>Mail</li>";
+                }
+                if (empty($_SESSION))
+                {
+                    if($page['mode'] != "Log In")
+                    {
+                        $nav_li .= "<li><a href=\"{$page['path']}login.php\">Log In</a></li>";
+                    }
+                    $nav_li .= "<li><a href=\"{$page['path']}register.php\">Register</a></li>";
+                }
+                if (!empty($_SESSION))
+                {
+                    $nav_li .= "<li><a href=\"{$page['path']}logout.php\">Log Out</a></li>";
+                }
+            ?>
             <ul>
                 <?php
-                    if (!empty($_SESSION['user']))
-                    {
-                        echo " <li><a href=\"{$page['path']}welcome.php\">HOME</a></li>
-                            <li><a href=\"{$page['path']}profile/\">Profile</a></li>";
-                    }
-                    if (!empty($_SESSION['profile']))
-                    {
-                        echo "<li><a href=\"{$page['path']}companies/\">Companies</a></li>
-                            <li><a href=\"{$page['path']}contacts/\">Contacts</a></li>
-                            <li><a href=\"{$page['path']}logs/\">Logs</a></li>
-                            <li>Mail</li>";
-                    }
-                    if (empty($_SESSION))
-                    {
-                        echo "<li><a href=\"{$page['path']}login.php\">Log In</a></li>
-                            <li><a href=\"{$page['path']}register.php\">Register</a></li>";
-                    }
-                    if (!empty($_SESSION))
-                    {
-                        echo "<li><a href=\"{$page['path']}logout.php\">Log Out</a></li>";
-                    }
+                    echo $nav_li;
                 ?>
             </ul>
         </nav>
