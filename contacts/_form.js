3392d8fa7b333fcca841ddee7355789fb6c3e13e
jephmann <jephmann@gmail.com>
2013-10-20 12:50:50 -0500

    jQuery required field validation for Contacts

---
+++ contacts/_form.js
@@ -0,0 +1,50 @@
+$(function() {
+    $( "#form_contact" ).validate({
+        rules: {
+            name_last: {
+                required: true
+            },
+            company: {
+                required: true
+            },
+            phone: {
+                digits: true,
+                minlength: 10,
+                maxlength: 10
+            },
+            phone_extension: {
+                digits: true,
+                minlength: 1,
+                maxlength: 4
+            },
+            fax: {
+                digits: true,
+                minlength: 10,
+                maxlength: 10
+            }
+        },
+        messages: {
+            name_last: {
+                required: "Contact: Last Name is required."
+            },
+            company: {
+                required: "Contact: Company is required."
+            },
+            phone: {
+                digits: "Numbers only for Phone",
+                minlength: $.format("Phone must be {0} digits."),
+                maxlength: $.format("Phone must be {0} digits.")
+            },
+            phone_extension: {
+                digits: "Numbers only for Phone Extension",
+                minlength: $.format("Phone Extension must be {0} digits."),
+                maxlength: $.format("Phone Extension must be {0} digits.")
+            },
+            fax: {
+                digits: "Numbers only for Fax",
+                minlength: $.format("Fax must be {0} digits."),
+                maxlength: $.format("Fax must be {0} digits.")
+            }
+        }
+    });
+});
--- contacts/_form.php
+++ contacts/_form.php
@@ -1,4 +1,4 @@
-<form method="post" action="">
+<form id="form_contact" method="post" action="">
     <fieldset>
         <legend>Contact</legend>
         <p>
@@ -90,3 +90,4 @@
         <input type="submit" value="<?php echo $page['mode']; ?>" />
     </div>
 </form>
+<script language="javascript" type="text/javascript" src="_form.js"></script>
