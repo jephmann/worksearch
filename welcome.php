a1b6c08b7939ed24cb4afb01aee7f29fa994bc9e
jephmann <jephmann@gmail.com>
2014-04-02 21:13:33 -0500

    revised contents: added provisions and explanations

--- welcome.php
+++ welcome.php
@@ -24,13 +24,30 @@
 ?>
 <fieldset>
     <legend>Contents</legend>
-    <ul>
-        <li><a href="profile/">Profile</a></li>
-        <li><a href="companies/">Companies</a></li>
-        <li><a href="contacts/">Contacts</a></li>
-        <li><a href="logs/">Logs</a></li>
-        <li>Mail</li>
-    </ul>
+    <h3><a href="profile/">Profile</a></h3>
+    <p>Quite simply: Who are you?</p>
+    <h3><a href="companies/">Companies</a></h3>
+    <p>These are the addresses of your prospects
+        -- companies with which you interviewed, companies where you might like to work, recruiting firms, etc.</p>
+    <h3><a href="contacts/">Contacts</a></h3>
+    <p>These are the people whom you would contact or people who initiated contact with you.
+        -- recruiters, H.R., maybe even CEOs.</p>
+    <h3><a href="logs/">Logs</a></h3>
+    <p>This is the actual activity which you had with your contacts, beyond merely adding them into the system.
+        Here you would log actual conversations, meetings, interviews and so on.
+        This section is partially modeled after the IDES Worksearch forms, from which this project "borrows" its working title.
+        To be added: creating a PDF of weekly or bi-weekly log reports.
+    </p>
+    <h3>Mail</h3>
+    <p>To be built. Compose emails, upload form-letter templates and revise them, prepare a targeted mass e-mail campaign.</p>
+    <h3>Documents</h3>
+    <p>To be built. A provision for resumes, cover letters and form-letter templates.</p>
+    <h3>Networking</h3>
+    <p>To be built. A diary of sorts regarding networking events you may have attended, including job fairs, MeetUps and the like,
+        wherein you might meet several people in one place.
+        Although logs such as the IDES Worksearch forms do not easily accommodate such information, any networking activity should
+        count as a credit toward your diligence in seeking work.
+    </p>
 </fieldset>
 <?php    
     require_once ($page['path'].'_views/footer.php');
