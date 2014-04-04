19d80fa1e2a37c1238508922ce65d1c95b35556e
jephmann <jephmann@gmail.com>
2014-04-03 23:01:20 -0500

    revised Contents

--- welcome.php
+++ welcome.php
@@ -23,31 +23,85 @@
     require_once ($page['path'].'_views/aside.php');
 ?>
 <fieldset>
-    <legend>Contents</legend>
-    <h3><a href="profile/">Profile</a></h3>
-    <p>Quite simply: Who are you?</p>
-    <h3><a href="companies/">Companies</a></h3>
-    <p>These are the addresses of your prospects
-        -- companies with which you interviewed, companies where you might like to work, recruiting firms, etc.</p>
-    <h3><a href="contacts/">Contacts</a></h3>
-    <p>These are the people whom you would contact or people who initiated contact with you.
-        -- recruiters, H.R., maybe even CEOs.</p>
-    <h3><a href="logs/">Logs</a></h3>
-    <p>This is the actual activity which you had with your contacts, beyond merely adding them into the system.
-        Here you would log actual conversations, meetings, interviews and so on.
-        This section is partially modeled after the IDES Worksearch forms, from which this project "borrows" its working title.
-        To be added: creating a PDF of weekly or bi-weekly log reports.
-    </p>
-    <h3>Mail</h3>
-    <p>To be built. Compose emails, upload form-letter templates and revise them, prepare a targeted mass e-mail campaign.</p>
-    <h3>Documents</h3>
-    <p>To be built. A provision for resumes, cover letters and form-letter templates.</p>
-    <h3>Networking</h3>
-    <p>To be built. A diary of sorts regarding networking events you may have attended, including job fairs, MeetUps and the like,
-        wherein you might meet several people in one place.
-        Although logs such as the IDES Worksearch forms do not easily accommodate such information, any networking activity should
-        count as a credit toward your diligence in seeking work.
-    </p>
+    <legend>WELCOME!</legend>
+        <div class="contents">
+            <span>
+                <a href="profile/">Profile</a>
+            </span>
+            <p>
+                Quite simply: Who are you?
+            </p>            
+        </div>
+        <div class="contents">
+            <span>
+                <a href="companies/">Companies</a>
+            </span>
+            <p>
+                These are the addresses of your prospects
+                -- companies with which you interviewed, companies where you
+                might like to work, recruiting firms, etc.
+                [I might actually rename this section "Prospects".]
+            </p>
+            <div class="clear"></div>
+        </div>
+        <div class="contents">
+            <span>
+                <a href="contacts/">Contacts</a>
+            </span>
+            <p>
+                These are the people whom you would contact or people who
+                initiated contact with you.
+                -- recruiters, H.R., maybe even CEOs.
+            </p>            
+        </div>
+        <div class="contents">
+            <span>
+                <a href="logs/">Logs</a>
+            </span>
+            <p>
+                This is the actual activity which you had with your contacts,
+                beyond merely adding them into the system.
+                Here you would log actual conversations, meetings, interviews
+                and so on.
+                This section is partially modeled after the IDES Worksearch
+                forms, from which this project "borrows" its working title.
+                To be added: creating a PDF of weekly or bi-weekly log reports.
+            </p>            
+        </div>
+        <div class="contents">
+            <span>
+                Mail
+            </span>
+            <p>
+                To be built.
+                Compose emails, upload form-letter templates and revise them,
+                prepare a targeted mass e-mail campaign.
+            </p>            
+        </div>
+        <div class="contents">
+            <span>
+                Documents
+            </span>
+            <p>
+                To be built.
+                A provision for resumes, cover letters and form-letter
+                templates.
+            </p>            
+        </div>
+        <div class="contents">
+            <span>
+                Networking
+            </span>
+            <p>
+                To be built.
+                A diary of sorts regarding networking events you may have
+                attended, including job fairs, MeetUps and the like, wherein you
+                might meet several people in one place.
+                Although logs such as the IDES Worksearch forms do not easily
+                accommodate such information, any networking activity should
+                count as a credit toward your diligence in seeking work.
+            </p>            
+        </div>
 </fieldset>
 <?php    
     require_once ($page['path'].'_views/footer.php');
