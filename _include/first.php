012c7eab237102e970404a1fe0b9f8f2fed67687
jephmann <jephmann@gmail.com>
2014-04-18 22:50:54 -0500

    class autoloading and other common tasks

--- _include/first.php
+++ _include/first.php
@@ -15,6 +15,43 @@
     require_once("{$path_config}pdo.php");  // common.php part 1 (PDO configs, which include basic db variables)
     require_once("{$path_config}page.php"); // common.php part 2 (page configs, from within and without the tutorial)
     
+    // autoloading class files    
+    spl_autoload_register(null, false);     /*** nullify any existing autoloads ***/    
+    spl_autoload_extensions('.class.php');  /*** specify extensions that may be loaded ***/    
+    function classLoader($class)            /*** class Loader ***/
+    {
+        $filename   = strtolower($class) . '.class.php';
+        $file       = '_classes/' . $filename;
+        if (!file_exists($file))
+        {
+            $file   = '../' . $file;
+            if (!file_exists($file))
+            {
+                return false;
+            }
+        }
+        include $file;
+    }    
+    spl_autoload_register('classLoader');   /*** register the loader functions ***/
+    
+    /* COMMON CLASSES AND RELATED FUNCTIONS */
+    // database class
+    $objData    = new Data;
+    // common helper classes
+    $formats    = new Format;
+    $inputs     = new Input;
+    $links      = new Link;
+    $objStatus  = new Status;
+    $validate   = new Validation;    
+    // common images
+    $link_img       = "{$page['path']}_images/SocialMediaBookmarkIcon/16/";
+    $img_linkedin   = $formats->image($link_img.'linkedin.png','LinkedIn');
+    $img_twitter    = $formats->image($link_img.'twitter.png','Twitter');
+    $img_facebook   = $formats->image($link_img.'facebook.png','Facebook');
+    $img_googleplus = $formats->image($link_img.'linkedin.png','Google+');    
+    // status message
+    $objStatus->setClass("status_quo");
+    
     /*
         Note that it is a good practice to NOT end your PHP files with a closing PHP tag.
         This prevents trailing newlines on the file from being included in your output,
