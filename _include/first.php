16b95ab6b4f95308f9ae959ac156cfdbce2706b2
jephmann <jephmann@gmail.com>
2014-05-19 21:38:58 -0500

    removed calls of helper classes

--- _include/first.php
+++ _include/first.php
@@ -35,19 +35,14 @@
     spl_autoload_register('classLoader');   /*** register the loader functions ***/
     
     /* COMMON CLASSES AND RELATED FUNCTIONS */
-    // database class
     $objData    = new Data;
-    // common helper classes
-    $formats    = new Format;
-    $inputs     = new Input;
-    $links      = new Link;
     $objStatus  = new Status;
     // common images
     $link_img       = "{$page['path']}_images/SocialMediaBookmarkIcon/16/";
-    $img_linkedin   = $formats->image($link_img.'linkedin.png','LinkedIn');
-    $img_twitter    = $formats->image($link_img.'twitter.png','Twitter');
-    $img_facebook   = $formats->image($link_img.'facebook.png','Facebook');
-    $img_googleplus = $formats->image($link_img.'linkedin.png','Google+');    
+    $img_linkedin   = Format::image($link_img.'linkedin.png','LinkedIn');
+    $img_twitter    = Format::image($link_img.'twitter.png','Twitter');
+    $img_facebook   = Format::image($link_img.'facebook.png','Facebook');
+    $img_googleplus = Format::image($link_img.'linkedin.png','Google+');    
     // status message
     $objStatus->setClass("status_quo");
     
